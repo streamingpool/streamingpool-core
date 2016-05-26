cfc52d1eff49a68c58b8f26e51e3551e3fbd915a
mgalilee <Az3rty?Qw3rtz>
2016-05-26 18:33:25 +0200

    idbasedsource & test

---
+++ src/java/stream/proto/akka/IdBasedSource.java
@@ -0,0 +1,110 @@
+package stream.proto.akka;
+
+import akka.stream.Attributes;
+import akka.stream.Outlet;
+import akka.stream.SourceShape;
+import akka.stream.stage.AbstractOutHandler;
+import akka.stream.stage.GraphStage;
+import akka.stream.stage.GraphStageLogic;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import stream.DiscoveryService;
+import stream.ReactStream;
+import stream.ReactStreams;
+import stream.StreamId;
+
+/**
+ * Created by mgalilee on 26/05/2016.
+ */
+public class IdBasedSource<T> extends GraphStage<SourceShape<T>> {
+
+    private final StreamId<T> streamId;
+    private final Outlet<T> out;
+
+    public IdBasedSource(StreamId<T> streamId) {
+        this.streamId = streamId;
+        this.out = Outlet.create("IdBasedSource_" + streamId.toString() + ".out");
+    }
+
+    @Override
+    public SourceShape<T> shape() {
+        return new SourceShape<>(out);
+    }
+
+    @Override
+    public GraphStageLogic createLogic(Attributes inheritedAttributes) {
+
+        SourceSubscriber subscriber = getSourceSubscriber(inheritedAttributes);
+
+        return new GraphStageLogic(shape()) {
+            {
+                setHandler(out, new AbstractOutHandler() {
+                    @Override
+                    public void onPull() throws Exception {
+                        push(out, subscriber.latestValue);
+                        if(subscriber.live) {
+                            subscriber.subscription.request(1); // TODO tune request amount smartly
+                        } else {
+                            completeStage();
+                        }
+                    }
+
+                    @Override
+                    public void onDownstreamFinish() throws Exception {
+                        super.onDownstreamFinish();
+                        subscriber.subscription.cancel();
+                    }
+                });
+            }
+
+        };
+    }
+
+    private SourceSubscriber getSourceSubscriber(Attributes inheritedAttributes) {
+        DiscoveryService service = getDiscoveryService(inheritedAttributes);
+        ReactStream<T> stream = service.discover(streamId);
+        Publisher<T> publisher = ReactStreams.publisherFrom(stream);
+        SourceSubscriber subscriber = new SourceSubscriber();
+        publisher.subscribe(subscriber);
+        return subscriber;
+    }
+
+    // from attributes? injected?
+    // currently package protected for testing purposes
+    protected DiscoveryService getDiscoveryService(Attributes inheritedAttributes) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    private class SourceSubscriber implements Subscriber<T> {
+
+        private Subscription subscription;
+        private T latestValue;
+        private boolean live = false;
+
+        @Override
+        public void onSubscribe(Subscription subscription) {
+            this.subscription = subscription;
+            subscription.request(1);
+            live = true;
+        }
+
+        @Override
+        public void onNext(T t) {
+            latestValue = t;
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            // TODO check reactive streams behavior, probably just log
+            live = false;
+        }
+
+        @Override
+        public void onComplete() {
+            // TODO check reactive streams behavior, probably just log
+            live = false;
+        }
+    }
+
+}
--- src/test/stream/AkkaIdSourceTest.java
+++ src/test/stream/AkkaIdSourceTest.java
@@ -4,79 +4,60 @@
 
 package stream;
 
-import static org.junit.Assert.fail;
-
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
-
-import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
 import akka.stream.Attributes;
-import akka.stream.Outlet;
-import akka.stream.SourceShape;
+import akka.stream.Materializer;
 import akka.stream.javadsl.Source;
-import akka.stream.stage.AbstractOutHandler;
-import akka.stream.stage.GraphStage;
-import akka.stream.stage.GraphStageLogic;
+import org.junit.Test;
+import rx.Observable;
+import stream.impl.SimplePool;
+import stream.proto.akka.IdBasedSource;
+
+import java.util.concurrent.CountDownLatch;
 
 public class AkkaIdSourceTest {
 
+    private static final StreamId<Integer> STREAM_ID = new StreamId<Integer>() {
+        @Override
+        public String toString() {
+            return "testIntegerStreamId";
+        }
+    };
+
+    private  static final SimplePool SIMPLE_POOL = new SimplePool();
+
+    static {
+        SIMPLE_POOL.provide(STREAM_ID, ReactStreams.fromRx(Observable.range(0, 1000)));
+    }
+
+    private static class TestIdBasedSource extends IdBasedSource<Integer> {
+
+        TestIdBasedSource() {
+            super(STREAM_ID);
+        }
+
+        @Override
+        protected DiscoveryService getDiscoveryService(Attributes attributes) {
+            return SIMPLE_POOL;
+        }
+    }
+
     @Test
-    public void createIdSource() {
-        Source<Object, NotUsed> source = Source.fromPublisher(null);
-        Assertions.assertThat(source).isNotNull();
+    public void createIdSource() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1000);
+
+        ActorSystem system = ActorSystem.create("AkkaIdSourceTest");
+        Materializer materializer = ActorMaterializer.create(system);
+
+        IdBasedSource<Integer> sourceGraph = new TestIdBasedSource();
+
+        Source.fromGraph(sourceGraph)
+                .runForeach(i -> {
+                    System.out.println(i);
+                    latch.countDown();
+                }, materializer);
+
+        latch.await();
     }
-
-    public class IdBasedSource<T> extends GraphStage<SourceShape<T>> {
-
-        private final Outlet<T> out = Outlet.create("NumberSource.out");
-        private final SourceShape<T> shape = SourceShape.of(out);
-
-        private final StreamId<T> streamId;
-
-        public IdBasedSource(StreamId<T> streamId) {
-            this.streamId = streamId;
-        }
-
-        @Override
-        public SourceShape<T> shape() {
-            return this.shape;
-        }
-
-        @Override
-        public GraphStageLogic createLogic(Attributes inheritedAttributes) {
-            DiscoveryService service = toBeImplemented();
-            ReactStream<T> stream = service.discover(streamId);
-
-            // lookup the stream and forward all the input to the outlet
-
-            final Source<T, NotUsed> publisherSource = Source.fromPublisher(ReactStreams.publisherFrom(stream));
-
-            return new GraphStageLogic(shape()) {
-                {
-                    setHandler(out, new AbstractOutHandler() {
-                        @Override
-                        public void onPull() throws Exception {
-                            // push(out, publisherSource.);
-                        }
-                    });
-                }
-
-            };
-        }
-
-        /**
-         * @return
-         */
-        private DiscoveryService toBeImplemented() {
-            return new DiscoveryService() {
-
-                @Override
-                public <T> ReactStream<T> discover(StreamId<T> id) {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-            };
-        } // to be gotten from attributes (hopefully ;-)
-    }
-
 }
