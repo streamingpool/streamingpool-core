c97cf35dd2e6c70c34d5ddb623f5dc11199ad1bf
acalia <andrea.calia@cern.ch>
2016-06-16 14:50:37 +0200

    fixes test due to refactor

--- src/test/cern/streaming/pool/core/service/impl/SimplePoolTest.java
+++ src/test/cern/streaming/pool/core/service/impl/SimplePoolTest.java
@@ -20,64 +20,63 @@ import cern.streaming.pool.core.service.StreamId;
  */
 @SuppressWarnings("unchecked")
 public class SimplePoolTest {
-    
+
     private static final StreamId<Object> ID_A = mock(StreamId.class);
     private static final StreamId<Object> ID_B = mock(StreamId.class);
+    private static final StreamId<Object> ID_NOT_PROVIDED = mock(StreamId.class);
     private static final ReactStream<Object> STREAM_A = mock(ReactStream.class);
     private static final ReactStream<Object> STREAM_B = mock(ReactStream.class);
-    
-    private final SimplePool pool = new SimplePool();
-    
+
+    private SimplePool pool;
+
     @Before
     public void setUp() {
-        pool.clearPool();
+        pool = new SimplePool();
         pool.provide(ID_A, STREAM_A);
     }
-    
+
     @Test(expected = NullPointerException.class)
     public void provideWithNullId() {
         pool.provide(null, null);
     }
-    
+
     @Test(expected = NullPointerException.class)
     public void provideWithNullStream() {
         pool.provide(ID_A, null);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void provideDuplicateSupplier() {
         pool.provide(ID_A, STREAM_A);
     }
-    
+
     @Test
     public void provideNewSupplier() {
         pool.provide(ID_B, STREAM_B);
         ReactStream<Object> stream = pool.discover(ID_B);
-        
+
         assertEquals(STREAM_B, stream);
     }
-    
+
     @Test(expected = NullPointerException.class)
     public void discoverNullId() {
         pool.discover(null);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void discoverUnavailableStream() {
         pool.discover(ID_B);
     }
-    
+
     @Test
     public void discoverAvailableStream() {
         ReactStream<Object> stream = pool.discover(ID_A);
-        
+
         assertEquals(STREAM_A, stream);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void testClear() {
-        pool.clearPool();
-        
-        pool.discover(ID_A);
+        pool.discover(ID_NOT_PROVIDED);
     }
 }
