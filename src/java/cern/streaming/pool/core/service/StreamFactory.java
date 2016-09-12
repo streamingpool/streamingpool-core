7ce8f0973043e44b4fe724d5f058f5f834d0999f
acalia <andrea.calia@cern.ch>
2016-09-12 10:26:20 +0200

    better doc

--- src/java/cern/streaming/pool/core/service/StreamFactory.java
+++ src/java/cern/streaming/pool/core/service/StreamFactory.java
@@ -6,12 +6,35 @@ package cern.streaming.pool.core.service;
 
 import java.util.Optional;
 
+/**
+ * This interface represents a factory for {@link ReactiveStream}s. An implementation of this interface is able to
+ * create a stream given an implementation of {@link StreamId} and a {@link DiscoveryService}. During the creation of a
+ * stream it is allowed to discover other streams. This allow the possibility to create streams that merge other streams
+ * while performing transformations.
+ * </p>
+ * <strong>NOTE</strong>: it is not allowed to discover other streams using the provided {@link DiscoveryService} in
+ * multiple threads. In other words, do not use new threads inside the stream creation. The provided
+ * {@link DiscoveryService} checks that subsequent discoveries are performed on the same thread, otherwise an exception
+ * is risen. It is not possible to enforce the single thread execution in the
+ * {@link #create(StreamId, DiscoveryService)} method, but using the {@link DiscoveryService} from different threads may
+ * lead to unpredictable behavior and can cause deadlocks.
+ * 
+ * @author acalia
+ */
 public interface StreamFactory {
 
-    /**
-     * @param id
-     * @param discoveryService
-     * @return
+    /***
+     * Given an implementation of {@link StreamId} and a {@link DiscoveryService} this method creates a
+     * {@link ReactiveStream<X>}. The provided {@link DiscoveryService} can be used to discover other streams that are
+     * needed in the creation process (stream combination, transformation, etc.)
+     * </p>
+     * <strong>NOTE</strong>: it is strongly discouraged the use of multiple threads inside this method (see
+     * {@link TypedStreamFactory} documentation).
+     * 
+     * @param id the id of the stream to create
+     * @param discoveryService {@link DiscoveryService} which can be used by the factory to look up other streams
+     *            ('upstream' of the one it will create)
+     * @return the newly created stream or {@code null} if this factory cannot create the stream of the given id
      */
     <T> Optional<ReactiveStream<T>> create(StreamId<T> id, DiscoveryService discoveryService);
 
--- src/java/cern/streaming/pool/core/service/TypedStreamFactory.java
+++ src/java/cern/streaming/pool/core/service/TypedStreamFactory.java
@@ -7,56 +7,52 @@ package cern.streaming.pool.core.service;
 import java.util.Optional;
 
 /**
- * This interface represents a factory for {@link ReactiveStream}s. An implementation of this interface is able to
- * create a stream given an implementation of {@link StreamId} and a {@link DiscoveryService}. During the creation of a
- * stream it is allowed to discover other streams. This allow the possibility to create streams that merge other streams
- * while performing transformations.
- * </p>
- * <strong>NOTE</strong>: it is not allowed to discover other streams using the provided {@link DiscoveryService} in
- * multiple threads. In other words, do not use new threads inside the stream creation. The provided
- * {@link DiscoveryService} checks that subsequent discoveries are performed on the same thread, otherwise an exception
- * is rose. It is not possible to enforce the single thread execution in the {@link #create(StreamId, DiscoveryService)}
- * method, but using the {@link DiscoveryService} from different threads may lead to unpredictable behavior and can
- * cause deadlocks.
+ * Typed version of a {@link StreamFactory}. A {@link StreamFactory} can create any type of streams, but sometimes there
+ * are scenarios in which the type of the {@link StreamId} and the data type of the {@link ReactiveStream} to be created
+ * are known. In those cases, {@link TypedStreamFactory} can simplify the development of a {@link StreamFactory}. The
+ * {@link StreamFactory#create(StreamId, DiscoveryService)} method is provided with a default implementation that suites
+ * most use cases.
+ * 
  * @param <X> The type of objects published by the {@link ReactiveStream} and therefore used by the {@link StreamId}
- *           implementation
+ *            implementation
  * @param <T> The class of {@link StreamId} to be used in the constrution of {@link ReactiveStream}s.
  * @author maosinsk
  * @author timartin
  */
-public interface TypedStreamFactory <X, T extends StreamId<X>> extends StreamFactory {
-    
+public interface TypedStreamFactory<X, T extends StreamId<X>> extends StreamFactory {
+
+    /**
+     * Default implementation of the {@link StreamFactory#create(StreamId, DiscoveryService)} method. Is uses
+     * {@link #createReactiveStream(StreamId, DiscoveryService)} and {@link #streamIdClass()} to provide a more
+     * developer friendly way of creating {@link ReactiveStream}s.
+     */
     @SuppressWarnings("unchecked")
     @Override
     default <Y> Optional<ReactiveStream<Y>> create(StreamId<Y> id, DiscoveryService discoveryService) {
-        if(!streamIdClass().isAssignableFrom(id.getClass())) return Optional.empty();
+        if (!streamIdClass().isAssignableFrom(id.getClass())) {
+            return Optional.empty();
+        }
 
         return Optional.of((ReactiveStream<Y>) createReactiveStream((T) id, discoveryService));
     }
 
+    /**
+     * Actually create the {@link ReactiveStream} from the given id. It is much like
+     * {@link StreamFactory#create(StreamId, DiscoveryService)} but with typed {@link StreamId}.
+     */
     ReactiveStream<X> createReactiveStream(T id, DiscoveryService discoveryService);
 
-    Class<T> streamIdClass();
-    
-    /***
-     * Given an implementation of {@link StreamId} and a {@link DiscoveryService} this method creates a
-     * {@link ReactiveStream<X>}. The provided {@link DiscoveryService} can be used to discover other streams that are
-     * needed in the creation process (stream combination, transformation, etc.)
-     * </p>
-     * <strong>NOTE</strong>: it is strongly discouraged the use of multiple threads inside this method (see
-     * {@link TypedStreamFactory} documentation).
-     * 
-     * @param id the id of the stream to create
-     * @param discoveryService {@link DiscoveryService} which can be used by the factory to look up other streams
-     *            ('upstream' of the one it will create)
-     * @return the newly created stream or {@code null} if this factory cannot create the stream of the given id
+    /**
+     * Returns the class that this {@link TypedStreamFactory} is able to create.
      */
+    Class<T> streamIdClass();
 
     /**
-     * Given a generic {@link StreamId} this method returns a {@link Boolean} indicating whether the {@link TypedStreamFactory}
-     * can create a {@link ReactiveStream}.
+     * Given a generic {@link StreamId} this method returns a {@link Boolean} indicating whether the
+     * {@link TypedStreamFactory} can create a {@link ReactiveStream}.
      *
      * @param id the {@link StreamId} of the {@link ReactiveStream} to be created.
-     * @return true if the factory can create a @{ReactiveStream} out of the provided @{link StreamId}, false, otherswise.
+     * @return true if the factory can create a @{ReactiveStream} out of the provided @{link StreamId}, false,
+     *         otherswise.
      */
 }
