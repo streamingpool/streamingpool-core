e18920fd33fea6ffd00764c17d327d6b69bf6773
Kajetan Fuchsberger <kajetan.fuchsberger@cern.ch>
2017-06-09 15:39:56 +0200

    removed log4j deps

--- src/test/org/streamingpool/core/service/AkkaSourceProvidingTest.java
+++ src/test/org/streamingpool/core/service/AkkaSourceProvidingTest.java
@@ -28,11 +28,8 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.streamingpool.core.testing.util.UncheckedWaits.waitFor;
 
-import org.apache.log4j.BasicConfigurator;
-import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.streamingpool.core.service.StreamId;
 import org.streamingpool.core.support.StreamCollectingSupport;
 import org.streamingpool.core.testing.AbstractAkkaStreamTest;
 import org.streamingpool.core.testing.NamedStreamId;
@@ -44,83 +41,77 @@ import scala.concurrent.duration.Duration;
 
 public class AkkaSourceProvidingTest extends AbstractAkkaStreamTest implements StreamCollectingSupport {
 
-	private static final Source<Integer, NotUsed> COUNTER_50_HZ = Source.range(1, 100)
-			.throttle(50, Duration.create(1, SECONDS), 1, shaping()).buffer(1, OverflowStrategy.dropBuffer());
+    private static final Source<Integer, NotUsed> COUNTER_50_HZ = Source.range(1, 100)
+            .throttle(50, Duration.create(1, SECONDS), 1, shaping()).buffer(1, OverflowStrategy.dropBuffer());
 
-	private static final StreamId<Integer> STREAM_ID = NamedStreamId.ofName("ticker");
+    private static final StreamId<Integer> STREAM_ID = NamedStreamId.ofName("ticker");
 
-	@BeforeClass
-	public static final void setUpBeforeClass() {
-		BasicConfigurator.configure();
-	}
+    @Ignore
+    @Test
+    public void provideMaterializedIsRunningFromTheBeginning() {
+        provide(COUNTER_50_HZ).materialized().as(STREAM_ID);
 
-	@Ignore
-	@Test
-	public void provideMaterializedIsRunningFromTheBeginning() {
-		provide(COUNTER_50_HZ).materialized().as(STREAM_ID);
+        waitFor(1500, MILLISECONDS);
 
-		waitFor(1500, MILLISECONDS);
+        /*
+         * The first emitted observable is always 1; It seems that this comes from the input buffer of the Sink when
+         * transformed to a publisher.
+         */
+        assertThat(firstEmittedItem()).isEqualTo(1);
 
-		/*
-		 * The first emitted observable is always 1; It seems that this comes
-		 * from the input buffer of the Sink when transformed to a publisher.
-		 */
-		assertThat(firstEmittedItem()).isEqualTo(1);
+        /*
+         * The second observed value should be higher than 50 after the wait, because the source starts ticking on
+         * registration (=materialization)
+         */
+        assertThat(secondEmittedItem()).isGreaterThanOrEqualTo(50);
+    }
 
-		/*
-		 * The second observed value should be higher than 50 after the wait,
-		 * because the source starts ticking on registration (=materialization)
-		 */
-		assertThat(secondEmittedItem()).isGreaterThanOrEqualTo(50);
-	}
+    @Ignore
+    @Test
+    public void provideUnmaterializedStartsRunningOnLookup() {
+        provide(COUNTER_50_HZ).as(STREAM_ID);
 
-	@Ignore
-	@Test
-	public void provideUnmaterializedStartsRunningOnLookup() {
-		provide(COUNTER_50_HZ).as(STREAM_ID);
+        waitFor(1500, MILLISECONDS);
 
-		waitFor(1500, MILLISECONDS);
+        /*
+         * The first emitted observable is always 1; It seems that this comes from the input buffer of the Sink when
+         * transformed to a publisher.
+         */
+        assertThat(firstEmittedItem()).isEqualTo(1);
 
-		/*
-		 * The first emitted observable is always 1; It seems that this comes
-		 * from the input buffer of the Sink when transformed to a publisher.
-		 */
-		assertThat(firstEmittedItem()).isEqualTo(1);
+        /*
+         * The second observed value should be quite small (we assume <5) even after a wait, because the source starts
+         * ticking on first lookup.
+         */
+        assertThat(secondEmittedItem()).isLessThan(5);
+    }
 
-		/*
-		 * The second observed value should be quite small (we assume <5) even
-		 * after a wait, because the source starts ticking on first lookup.
-		 */
-		assertThat(secondEmittedItem()).isLessThan(5);
-	}
+    @Test
+    public void subscribeTwiceOnMaterializedProvidedIsPossible() {
+        provide(COUNTER_50_HZ).materialized().as(STREAM_ID);
+        assertSubscribeTwiceIsPossible();
+    }
 
-	@Test
-	public void subscribeTwiceOnMaterializedProvidedIsPossible() {
-		provide(COUNTER_50_HZ).materialized().as(STREAM_ID);
-		assertSubscribeTwiceIsPossible();
-	}
+    @Test
+    public void subscribeTwiceOnUnmaterializedProvidedIsPossible() {
+        provide(COUNTER_50_HZ).as(STREAM_ID);
+        assertSubscribeTwiceIsPossible();
+    }
 
-	@Test
-	public void subscribeTwiceOnUnmaterializedProvidedIsPossible() {
-		provide(COUNTER_50_HZ).as(STREAM_ID);
-		assertSubscribeTwiceIsPossible();
-	}
+    private Integer secondEmittedItem() {
+        return from(STREAM_ID).skip(1).and().awaitNext();
+    }
 
-	private Integer secondEmittedItem() {
-		return from(STREAM_ID).skip(1).and().awaitNext();
-	}
+    private Integer firstEmittedItem() {
+        return from(STREAM_ID).awaitNext();
+    }
 
-	private Integer firstEmittedItem() {
-		return from(STREAM_ID).awaitNext();
-	}
-
-	private void assertSubscribeTwiceIsPossible() {
-		rxFrom(STREAM_ID).subscribe((val) -> System.out.println("A:" + val));
-		/*
-		 * This call would throw, if the source would not have been materialized
-		 * with a sink with fanout
-		 */
-		rxFrom(STREAM_ID).blockingFirst();
-	}
+    private void assertSubscribeTwiceIsPossible() {
+        rxFrom(STREAM_ID).subscribe((val) -> System.out.println("A:" + val));
+        /*
+         * This call would throw, if the source would not have been materialized with a sink with fanout
+         */
+        rxFrom(STREAM_ID).blockingFirst();
+    }
 
 }
