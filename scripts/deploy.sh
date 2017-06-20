9b1aa4dc23edd47e23dc79df4da3cdaf8bb91922
Andrea Calia <andrea.calia.46@gmail.com>
2017-06-20 16:31:19 +0200

    travis verbose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e # exit with nonzero exit code if anything fails
-
+echo "<TRAVIS_TAG>"
+echo $TRAVIS_TAG
+echo "</TRAVIS-TAG>"
 ./gradlew bintrayUpload -DDEPLOYMENT=1 --info
