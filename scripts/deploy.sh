3509f0fed7698c9f337a9cb42080cd09b7b4836b
Andrea Calia <andrea.calia.46@gmail.com>
2017-06-20 17:06:34 +0200

    correct handling of system properties on deploy script

--- scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e # exit with nonzero exit code if anything fails
-echo "<TRAVIS_TAG>"
-echo $TRAVIS_TAG
-echo "</TRAVIS-TAG>"
-./gradlew bintrayUpload -DDEPLOYMENT=1 --info
