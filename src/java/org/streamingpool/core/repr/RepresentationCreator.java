b85ab5c2fd7fcc095a553ac0fa20b900e00f91a3
Kajetan Fuchsberger <kajetan.fuchsberger@cern.ch>
2017-08-02 14:53:26 +0200

    deleted unused classes

--- src/java/org/streamingpool/core/repr/RepresentationCreator.java
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2017 European Organisation for Nuclear Research (CERN), All Rights Reserved.
- */
-
-package org.streamingpool.core.repr;
-
-public interface RepresentationCreator {
-
-}
