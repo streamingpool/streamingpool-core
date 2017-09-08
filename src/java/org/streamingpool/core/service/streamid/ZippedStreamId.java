d885df91506d5c492d5f54d34907b406ee374112
makemeunsee <makemeunsee@gmail.com>
2017-09-08 11:33:56 +0200

    simpler & safer zip stream id

--- src/java/org/streamingpool/core/service/streamid/ZippedStreamId.java
+++ src/java/org/streamingpool/core/service/streamid/ZippedStreamId.java
@@ -1,57 +1,71 @@
 package org.streamingpool.core.service.streamid;
 
+import io.reactivex.functions.BiFunction;
 import org.streamingpool.core.service.StreamId;
 import org.streamingpool.core.service.streamid.factory.function.ZipCompositionFunction;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import io.reactivex.functions.Function;
 
 import static java.util.Objects.requireNonNull;
 
 /**
  * Zips the items of the stream with the {@link ZipCompositionFunction}.
- * @param <S> type of the source stream
+ *
+ * @param <S1> type of the first source stream
+ * @param <S2> type of the second source stream
  * @param <T> type that function holds
  */
-public class ZippedStreamId<S, T> implements StreamId<T> {
+public class ZippedStreamId<S1, S2, T> implements StreamId<T> {
     private static final long serialVersionUID = 1L;
-    private final Iterable<StreamId<S>> sourceStreamIds;
-    private final Function<Object[], Optional<T>> function;
+    private final StreamId<S1> sourceStreamId1;
+    private final StreamId<S2> sourceStreamId2;
+    private final BiFunction<S1, S2, Optional<T>> function;
 
-    private ZippedStreamId(Iterable<StreamId<S>> sourceStreamIds, Function<Object[], Optional<T>> function) {
-        this.sourceStreamIds = sourceStreamIds;
+    private ZippedStreamId(StreamId<S1> sourceStreamId1, StreamId<S2> sourceStreamId2,
+                           BiFunction<S1, S2, Optional<T>> function) {
+        this.sourceStreamId1 = sourceStreamId1;
+        this.sourceStreamId2 = sourceStreamId2;
         this.function = requireNonNull(function, "function must not be null");
     }
 
-    public static <S, T> ZippedStreamId<S, T> zip(Iterable<StreamId<S>> streamIds,  Function<Object[], Optional<T>> function){
-        Objects.requireNonNull(streamIds, "streamIds must not be null");
-        return new ZippedStreamId<>(streamIds, function);
+    public static <S1, S2, T> ZippedStreamId<S1, S2, T> zip(StreamId<S1> sourceStreamId1, StreamId<S2> sourceStreamId2,
+                                                            BiFunction<S1, S2, Optional<T>> function) {
+        Objects.requireNonNull(sourceStreamId1, "sourceStreamId1 must not be null");
+        Objects.requireNonNull(sourceStreamId2, "sourceStreamId2 must not be null");
+        return new ZippedStreamId<>(sourceStreamId1, sourceStreamId2, function);
     }
 
-    public Iterable<StreamId<S>> sourceStreamIds() { return sourceStreamIds; }
+    public StreamId<S1> sourceStreamId1() {
+        return sourceStreamId1;
+    }
 
-    public Function<Object[], Optional<T>> function() {
+    public StreamId<S2> sourceStreamId2() {
+        return sourceStreamId2;
+    }
+
+    public BiFunction<S1, S2, Optional<T>> function() {
         return function;
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(final Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        ZippedStreamId<?, ?> that = (ZippedStreamId<?, ?>) o;
+        ZippedStreamId<?, ?, ?> that = (ZippedStreamId<?, ?, ?>) o;
 
-        if (sourceStreamIds != null ? !sourceStreamIds.equals(that.sourceStreamIds) : that.sourceStreamIds != null)
+        if (sourceStreamId1 != null ? !sourceStreamId1.equals(that.sourceStreamId1) : that.sourceStreamId1 != null)
+            return false;
+        if (sourceStreamId2 != null ? !sourceStreamId2.equals(that.sourceStreamId2) : that.sourceStreamId2 != null)
             return false;
         return function != null ? function.equals(that.function) : that.function == null;
     }
 
     @Override
     public int hashCode() {
-        int result = sourceStreamIds != null ? sourceStreamIds.hashCode() : 0;
+        int result = sourceStreamId1 != null ? sourceStreamId1.hashCode() : 0;
+        result = 31 * result + (sourceStreamId2 != null ? sourceStreamId2.hashCode() : 0);
         result = 31 * result + (function != null ? function.hashCode() : 0);
         return result;
     }
@@ -59,11 +73,9 @@ public class ZippedStreamId<S, T> implements StreamId<T> {
     @Override
     public String toString() {
         return "ZippedStreamId{" +
-                "sourceStreamIds=" + sourceStreamIds +
+                "sourceStreamId1=" + sourceStreamId1 +
+                ", sourceStreamId2=" + sourceStreamId2 +
                 ", function=" + function +
                 '}';
     }
 }
-
-
-
